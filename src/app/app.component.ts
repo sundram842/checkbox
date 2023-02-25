b95ee07a8a8e251fd5e72be86d4e994942e4b9d3
sundram <sundramkumar773@gmail.com>
2023-02-25 11:58:00 +0530

    first change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'checkbox';
+change:boolean=false;
+  fruit=[{id:1,select:false,name:"apple"},
+    { id: 2, select: true, name: "banana" },
+    { id: 3, select: false, name: "grapes" },
+    { id: 5, select: true, name: "orange" },
+    { id: 6, select: false, name: "pinapple" },
+]
+
+  Onchange(event:any)
+  {
+     const all=event.target.value;
+     const isChecked=event.target.checked
+   
+     this.fruit= this.fruit.map((x)=>
+     {
+      
+      if(x.id==all)
+      {
+        this.change=false
+        x.select=isChecked; 
+        
+        return x;
+        
+      }
+       if (all == -1) {
+        x.select=this.change
+        return x;
+
+       }
+       return x;
+     });  
+    console.log(this.fruit);
+  }
+
 }
